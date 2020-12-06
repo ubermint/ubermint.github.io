73f1f3c500679265b030cbdcf616f4b900b1a576
M1neskA <vadim13tv@gmail.com>
2020-12-06 16:32:46 +0200

    some update

---
+++ js/map.js
@@ -0,0 +1,25 @@
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([8.40, 50.06]),
+    zoom: 10
+  })
+});
+
+var map2 = new ol.Map({
+  target: 'map2',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([8.40, 50.06]),
+    zoom: 10
+  })
+});
